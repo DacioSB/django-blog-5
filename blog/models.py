d4b74c10adfb75d7c8e7d623e5275ce4e5ce8017
dacio.bezerra <dacio.bezerra@dataprev.gov.br>
2026-01-21 11:27:18 -0300

    feature - :label: add Post model with status and ordering, include blog app in settings

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,30 @@
-from datetime import timezone
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 
-class Blog(models.Model):
+class Post(models.Model):
+    class Status (models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title

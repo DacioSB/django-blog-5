816c64d522d2f8973ae82c67755864f5adbc25eb
dacio.bezerra <dacio.bezerra@dataprev.gov.br>
2026-02-19 13:28:24 -0300

    feature - :label: implement Post model with author and slug fields, add migrations for initial setup

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -9,7 +10,12 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)

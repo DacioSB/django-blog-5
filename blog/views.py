ca6585354d560dafc8d7f5a6bf7dbdc6c3cef08a
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2026-06-06 13:10:39 -0300

    feature - :sparkles: update Post model slug field to be unique for publish date and modify post_detail view to use slug in URL

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
 # Create your views here.
@@ -7,9 +7,9 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     try:
-        post = Post.published.get(id=id)
+        post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day)
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
     return render(request, 'blog/post/detail.html', {'post': post})

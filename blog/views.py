11ca78536158e2559a25bae5fc1ae71e6f027ada
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2026-08-05 11:34:00 -0300

    add pagination as intended by django

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,17 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
 # Create your views here.
 
 def post_list(request):
